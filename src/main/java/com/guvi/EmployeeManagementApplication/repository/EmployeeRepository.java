07bfcdc9fce8580d33a8e5cc14f59172b18caa53
pppavithra <pavithraperiyasamy.3998@gmail.com>
2024-05-23 22:26:28 +0530

    Added JFS project

---
+++ src/main/java/com/guvi/EmployeeManagementApplication/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.guvi.EmployeeManagementApplication.repository;
+
+import com.guvi.EmployeeManagementApplication.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
